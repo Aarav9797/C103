a683bd1b727137664f2d18e5dfcd0d25a003160f
Aarav9797 <71883558+Aarav9797@users.noreply.github.com>
2021-02-11 16:57:54 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+var classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/Eof6uq_2e/model.json", modelloaded);
 Webcam.set({
     width: 300,
     height: 250,
@@ -5,9 +6,24 @@ Webcam.set({
     png_quality: 100,
 });
 Webcam.attach('camerA');
-function capture_image(){
-    Webcam.snap( function(datauri){
-        document.getElementById("resulTs").innerHTML="<img src='"+datauri+"'/>";
+
+function capture_image() {
+    Webcam.snap(function (datauri) {
+        document.getElementById("resulTs").innerHTML = "<img id='imgg' src='" + datauri + "'/>";
     })
-    document.getElementsById("resulTs").style.display="block"
+    document.getElementById("resulTs").style.display = "block";
+}
+console.log("The version is", ml5.version);
+
+function modelloaded() {
+    console.log("model has been loaded");
+}
+
+function identify() {
+    classifier.classify(document.getElementById("imgg"), check);
+}
+function check(error,results){
+    console.log(results);
+    document.getElementById("Ob").innerHTML=results[0].label;
+    document.getElementById("Ac").innerHTML=results[0].confidence;
 }
